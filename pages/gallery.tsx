06cba7674ce9a87244089585a8266d08de750291
Henil Thaker <henilthaker2010@gmail.com>
2023-03-31 23:17:51 +0530

    added carousel to gallery

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,13 +1,34 @@
 import Head from "next/head";
-export default function Gallery(){
-    return(
+import Image from "next/image";
+export default function Gallery() {
+    return (
         <>
-        <Head>
-            <title>Pandyaji Matrugaya | Gallery</title>
-        </Head>
-        <div className="container">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eius optio consequatur sed, doloremque culpa laborum cum esse et placeat omnis expedita harum laboriosam blanditiis iste soluta nisi eaque adipisci velit consequuntur tempore quasi saepe ad asperiores? Itaque culpa tempore quaerat facere sit, odio vel, exercitationem adipisci nobis fugiat quas saepe quam fuga. Asperiores nemo culpa quae, pariatur consectetur ad repudiandae, corrupti fugit id tenetur reiciendis, provident velit modi necessitatibus sapiente tempora cumque totam! Cupiditate at voluptates nostrum a suscipit ad unde quas nemo modi debitis esse, dignissimos numquam sit deserunt, cum minus ducimus voluptate repellendus assumenda consequuntur veniam exercitationem ipsum. Tempora commodi veniam dolorem repudiandae, aliquam, corporis minima eaque, quod perferendis aspernatur nesciunt magnam error veritatis nobis hic consectetur iure esse expedita sint inventore quasi exercitationem dolor ducimus. Cum ad accusamus eos sed vel, debitis nisi ab voluptatem id, quas quod! Dolorum veniam rem atque laudantium commodi eum et! Voluptatibus illum, facere quod aperiam ipsum ratione sunt voluptate sequi eligendi a fuga repellendus aliquam, rerum quasi molestiae temporibus ullam quisquam exercitationem ad mollitia molestias similique excepturi maxime dolorem! Provident possimus aperiam error hic, vitae officiis praesentium voluptatum ab eaque quasi distinctio recusandae nostrum repellat eius temporibus est nam, expedita porro totam molestiae repudiandae fuga? Fuga, delectus ab. A qui sapiente, veniam vel omnis ex nulla aliquam perspiciatis sunt autem quisquam voluptatum eaque consectetur deleniti, temporibus laudantium mollitia labore voluptatem fugiat ratione quaerat at? Expedita ratione minima doloremque illo? Laudantium ratione sapiente iure similique ab saepe mollitia recusandae modi quis?
-        </div>
+            <Head>
+                <title>Pandyaji Matrugaya | Gallery</title>
+            </Head>
+            <div className="container">
+                <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active" data-bs-interval="2000">
+                            <Image src="/images/img1.jpeg" className="d-block w-100" alt="..." width="100" height="275" />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <Image src="/images/img2.jpeg" className="d-block w-100" alt="..." width="100" height="275" />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <Image src="/images/img3.jpeg" className="d-block w-100" alt="..." width="100" height="275" />
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </>
     )
 }
