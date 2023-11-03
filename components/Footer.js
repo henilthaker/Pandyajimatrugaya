1aecc07279e8fe13f5804c314b080114b074902d
Henil Thaker <henilthaker2010@gmail.com>
2023-11-03 19:38:38 +0530

    changed navbar and footer theme

--- components/Footer.js
+++ components/Footer.js
@@ -1,57 +1,67 @@
 import Link from 'next/link';
 const Footer = () => {
     return (
-        <div className="container">
-            <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5">
-                <div className="col mb-3">
-                    <Link href="/" className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
-                        Pandyaji Matrugaya
-                    </Link>
-                    <p className="text-body-secondary">© 2023</p>
-                </div>
+        // <div className="container" data-bs-theme="dark">
+        //     <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5">
+        //         <div className="col mb-3">
+        //             <Link href="/" className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+        //                 Pandyaji Matrugaya
+        //             </Link>
+        //             <p className="text-body-secondary">© 2023</p>
+        //         </div>
 
-                <div className="col mb-3">
+        //         {/* <div className="col mb-3">
 
-                </div>
+        //         </div> */}
 
-                <div className="col mb-3">
-                    <h5>Quick Links</h5>
-                    <ul className="nav flex-column">
-                        <li className="nav-item mb-2"><Link href="/" className="nav-link p-0 text-body-secondary">Home</Link></li>
-                        <li className="nav-item mb-2"><Link href="/gallery" className="nav-link p-0 text-body-secondary">Gallery</Link></li>
-                        <li className="nav-item mb-2"><Link href="/matrugaya" className="nav-link p-0 text-body-secondary">Matrugaya</Link></li>
-                        <li className="nav-item mb-2"><Link href="/about-siddhpur" className="nav-link p-0 text-body-secondary">About Siddhpur</Link></li>
-                        <li className="nav-item mb-2"><Link href="/our-services" className="nav-link p-0 text-body-secondary">Our Services</Link></li>
-                        <li className="nav-item mb-2"><Link href="/contact" className="nav-link p-0 text-body-secondary">Contact</Link></li>
-                    </ul>
-                </div>
+        //         <div className="col mb-3">
+        //             <h5>Quick Links</h5>
+        //             <ul className="nav flex-column">
+        //                 <li className="nav-item mb-2"><Link href="/" className="nav-link p-0 text-body-secondary">Home</Link></li>
+        //                 <li className="nav-item mb-2"><Link href="/gallery" className="nav-link p-0 text-body-secondary">Gallery</Link></li>
+        //                 <li className="nav-item mb-2"><Link href="/matrugaya" className="nav-link p-0 text-body-secondary">Matrugaya</Link></li>
+        //                 <li className="nav-item mb-2"><Link href="/about-siddhpur" className="nav-link p-0 text-body-secondary">About Siddhpur</Link></li>
+        //                 <li className="nav-item mb-2"><Link href="/our-services" className="nav-link p-0 text-body-secondary">Our Services</Link></li>
+        //                 <li className="nav-item mb-2"><Link href="/contact" className="nav-link p-0 text-body-secondary">Contact</Link></li>
+        //             </ul>
+        //         </div>
 
-                <div className="col mb-3">
-                    <h5>Contact Details</h5>
-                    {/* <ul className="nav flex-column">
-                        <li className="nav-item mb-2">Phone: 9925072800</li>
-                        <li className="nav-item mb-2">Email: jigneshpandya@gmail.com</li>
-                        <li className="nav-item mb-2">Address: Bindusarovar, Siddhpur, Gujarat, India</li>
-                    </ul> */}
-                    <table cellPadding={10}>
-                        <tbody>
-                            <tr>
-                                <td><i className="fa fa-phone fa-lg"></i></td>
-                                <td>9925072800</td>
-                            </tr>
-                            <tr>
-                                <td><i className="fa fa-envelope"></i></td>
-                                <td>jigneshpandya@gmail.com</td>
-                            </tr>
-                            <tr>
-                                <td><i className="fa fa-map-marker fa-lg"></i></td>
-                                <td>Bindusarovar, Siddhpur, Gujarat, India</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+        //         <div className="col mb-3">
+        //             <h5>Contact Details</h5>
+        //             {/* <ul className="nav flex-column">
+        //                 <li className="nav-item mb-2">Phone: 9925072800</li>
+        //                 <li className="nav-item mb-2">Email: jigneshpandya@gmail.com</li>
+        //                 <li className="nav-item mb-2">Address: Bindusarovar, Siddhpur, Gujarat, India</li>
+        //             </ul> */}
+        //             <table cellPadding={10}>
+        //                 <tbody>
+        //                     <tr>
+        //                         <td><i className="fa fa-phone fa-lg"></i></td>
+        //                         <td>9925072800</td>
+        //                     </tr>
+        //                     <tr>
+        //                         <td><i className="fa fa-envelope"></i></td>
+        //                         <td>jigneshpandya@gmail.com</td>
+        //                     </tr>
+        //                     <tr>
+        //                         <td><i className="fa fa-map-marker fa-lg"></i></td>
+        //                         <td>Bindusarovar, Siddhpur, Gujarat, India</td>
+        //                     </tr>
+        //                 </tbody>
+        //             </table>
+        //         </div>
+        //     </footer>
+        // </div>
+            <footer className="py-3 my-4 my-footer" data-bs-theme="dark">
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Home</a></li>
+                    <li className="nav-item"><a href="/gallery" className="nav-link px-2 text-body-secondary">Gallery</a></li>
+                    <li className="nav-item"><a href="/matrugaya" className="nav-link px-2 text-body-secondary">Matrugaya</a></li>
+                    <li className="nav-item"><a href="/about-siddhpur" className="nav-link px-2 text-body-secondary">About Siddhpur</a></li>
+                    <li className="nav-item"><a href="/contact" className="nav-link px-2 text-body-secondary">Contact</a></li>
+                </ul>
+                <p className="text-center text-body-secondary">© 2023 Pandyaji Matrugaya</p>
             </footer>
-        </div>
     );
 }
 export default Footer;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,38 +1,38 @@
 import Link from 'next/link';
 const Navbar = () => {
     return (
-        <div className="container">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" href="/">Pandyaji Matrugaya</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" href="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/gallery">Gallery</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/matrugaya">Matrugaya</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/about-siddhpur">About Siddhpur</Link>
-                            </li>
-                            <li className="nav-item">
+
+        <nav className="navbar navbar-expand-lg bg-body-tertiary px-4" data-bs-theme="dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" href="/">Pandyaji Matrugaya</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" href="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/gallery">Gallery</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/matrugaya">Matrugaya</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/about-siddhpur">About Siddhpur</Link>
+                        </li>
+                        {/* <li className="nav-item">
                                 <Link className="nav-link" href="/our-services">Our Services</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/contact">Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
+                            </li> */}
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/contact">Contact</Link>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
+
     )
 }
 export default Navbar;
