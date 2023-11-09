aef345de27d9ff31e291cf20f91aae4321ed81c1
Henil Thaker <henilthaker2010@gmail.com>
2023-11-09 19:56:24 +0530

    changed title of contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -11,7 +11,7 @@ export default function Contact() {
     return (
         <>
             <Head>
-                <title>Pandyaji Matrugaya | Contact</title>
+                <title>Siddhpur Matrugaya Purohit | Contact</title>
             </Head>
             <div className="container my-4">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58351.333799760956!2d72.35030274715746!3d23.926528451205307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395cf4fdb8907cdb%3A0xb588efce07ef80e5!2sSiddhpur%2C%20Gujarat%20384151!5e0!3m2!1sen!2sin!4v1680359405268!5m2!1sen!2sin" className={styles.map} loading={"lazy"}>
