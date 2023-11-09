70349f10b31aff8427f6f5a04a66fedf660de004
Henil Thaker <henilthaker2010@gmail.com>
2023-11-09 12:36:51 +0530

    changed font and added logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image';
 const Navbar = () => {
     return (
 
         <nav className="navbar navbar-expand-lg bg-body-tertiary px-4" data-bs-theme="dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" href="/">Siddhpur Matrugaya Purohit</Link>
+                <Link className="navbar-brand" href="/"><Image src="/logo.png" height="50" width="220"/></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
