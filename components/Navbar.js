b1bfda55c247171fadf609fc8b7ec72b92835ebc
Henil Thaker <henilthaker2010@gmail.com>
2023-11-09 13:21:04 +0530

    addede logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
 
         <nav className="navbar navbar-expand-lg bg-body-tertiary px-4" data-bs-theme="dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" href="/"><Image src="/logo.png" height="50" width="220"/></Link>
+                <Link className="navbar-brand" href="/"><Image src="/finalLogo.png" height="50" width="220"/></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
